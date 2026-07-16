a67a3dd38c40f9d879963d7d4f17c0a0c318d2d1
Pistikepova69 <biro.gergo.t@gmail.com>
2026-07-17 00:47:12 +0200

    Minimális működő low budget játék.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,36 @@
 // clang-format off
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
 // clang-format on
 
+#include "ball.h"
 #include "bar.h"
-#include "circle.h"
-#include "rectangle.h"
 #include "win.h"
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action,
                   int mods);
 
 Win window;
-Bar b{0.05, 0.2, 1.5, 1, 0, 0, window, GLFW_KEY_W, GLFW_KEY_S, 1};
+std::vector<Bar> bars{
+    {0.05, 0.5, 1.7, 1, 0, 0, window, GLFW_KEY_UP, GLFW_KEY_DOWN, 2},
+    {0.05, 0.5, -1.7, 1, 0, 0, window, GLFW_KEY_W, GLFW_KEY_S, 2}};
+    
+    int main() {
+        window.set_key_callback(key_callback);
+        Ball ball(0.03, 1, 1, 0, window, 0.05);
 
-int main() {
-    window.set_key_callback(key_callback);
 
 
     while(!glfwWindowShouldClose(window)) {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        b.render();
-
+        for(Bar& bar : bars) {
+            bar.render();
+        }
+        ball.update(bars);
+        ball.render();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -34,5 +41,7 @@ int main() {
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action,
                   int mods) {
-    b.handle_input(key, action);
+    for(Bar& bar : bars) {
+        bar.handle_input(key, action);
+    }
 }
